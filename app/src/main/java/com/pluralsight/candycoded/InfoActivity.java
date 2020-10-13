de5e84d2354bd6959eb2fc1f6507e801cf37fefb
Siyammukela Kasapo <siyammukela@gmail.com>
2020-10-14 00:40:55 +0200

    Start the Phone Activity

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -43,4 +43,9 @@ public class InfoActivity extends AppCompatActivity {
     // ***
     // TODO - Task 3 - Launch the Phone Activity
     // ***
+    public void createPhoneIntent(View view){
+        Intent actionDial = new Intent(Intent.ACTION_DIAL);
+        actionDial.setData(Uri.parse("tel:0123456789"));
+        startActivity(actionDial);
+    }
 }
